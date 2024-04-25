ed908d45da229337a9d14d060b889b675cf1cb61
Pedro Bueno <pedroarcbueno@gmail.com>
2024-04-25 15:58:38 -0300

    feat: validation and destroy in departments

--- rheoli_cyfoeth/setup/serializer.py
+++ rheoli_cyfoeth/setup/serializer.py
@@ -4,16 +4,6 @@ from setup.models.department import Department
 from setup.models.item import Item
 from django.contrib.auth.models import User
 
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Item
-        fields = '__all__'
-        
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Department
-        fields = '__all__'
-        
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
@@ -30,6 +20,19 @@ class GetUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'is_staff', 'first_name', 'last_name', ]
+        
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Item
+        fields = '__all__'
+        
+class DepartmentSerializer(serializers.ModelSerializer):
+    manager = GetUserSerializer()
+    
+    class Meta:
+        model= Department
+        fields = '__all__'
         
 class DepartmentItemListSerializer(serializers.ModelSerializer):
     department = serializers.ReadOnlyField(source='department.description')
--- rheoli_cyfoeth/setup/views/department_view.py
+++ rheoli_cyfoeth/setup/views/department_view.py
@@ -1,11 +1,52 @@
-from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 
 from setup.models.department import Department
 from setup.serializer import DepartmentSerializer
+
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 class DepartmentView(viewsets.ModelViewSet):
     
     queryset = Department.objects.all();
     serializer_class = DepartmentSerializer
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
+    
+    # Override
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'destroy':
+            permission_classes = [IsAdminUser, IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+    
+    # Override
+    def destroy(self, request: Request, pk=None) -> Response:
+        """
+        Delete a department by id.
+        """
+        try:
+            if(pk == None): 
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            department = Department.objects.get(id=pk)
+            Department.delete(department)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Department.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def retrieve(self, request: Request, pk=None) -> Response:
+        """
+        Get a department by id.
+        """
+        try:
+            if(pk == None): 
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            department = Department.objects.filter(id=pk).prefetch_related('manager').first()
+            serialized = DepartmentSerializer(department)
+            return Response(serialized.data,status=status.HTTP_200_OK, )
+        except Department.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
